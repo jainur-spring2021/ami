7fbcd8a895f8ba585fcb6a39998d2845d04ba9f5
urvashijain18 <jain.ur@husky.neu.edu>
2021-03-16 15:56:08 -0400

    build

--- packer/buildAmi.sh
+++ packer/buildAmi.sh
@@ -6,5 +6,4 @@ packer build \
     -var 'subnet_id=subnet-ac103ce1' \
     -var 'source_ami=ami-03d315ad33b9d49c4' \
     -var 'ami_users=205467980008' \
-    -var 'owner=099720109477' \
     packer/ami.json
